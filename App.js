1f55db4b85cb37ec843a183d0bba9f569f7c1248
Serhii Konvisar <s.v.konvisar@gmail.com>
2018-12-28 14:53:38 +0200

    Add firebase clound messaging

--- App.js
+++ App.js
@@ -1,18 +1,90 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import firebase from "react-native-firebase";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 export default class App extends Component {
+  async componentDidMount() {
+    const NotificationOpen = await firebase
+      .notifications()
+      .getInitialNotification();
+    if (NotificationOpen) {
+      const action = NotificationOpen.action;
+      const Notification = NotificationOpen.notification;
+      var seen = [];
+      alert(
+        JSON.stringify(Notification.data, function(key, val) {
+          if (val != null && typeof val == "object") {
+            if (seen.indexOf(val) >= 0) {
+              return;
+            }
+            seen.push(val);
+          }
+          return val;
+        })
+      );
+    }
+    const channel = new firebase.notifications.Android.Channel(
+      "test-channel",
+      "Test Channel",
+      firebase.notifications.Android.Importance.Max
+    ).setDescription("My apps test channel");
+    // Create the channel
+    firebase.notifications().android.createChannel(channel);
+    this.notificationDisplayedListener = firebase
+      .notifications()
+      .onNotificationDisplayed(Notification => {
+        // Process your notification as required
+        // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
+      });
+    this.notificationListener = firebase
+      .notifications()
+      .onNotification(Notification => {
+        // Process your notification as required
+        Notification.android
+          .setChannelId("test-channel")
+          .android.setSmallIcon("ic_launcher");
+        firebase.notifications().displayNotification(Notification);
+      });
+    this.notificationOpenedListener = firebase
+      .notifications()
+      .onNotificationOpened(NotificationOpen => {
+        // Get the action triggered by the notification being opened
+        const action = NotificationOpen.action;
+        // Get information about the notification that was opened
+        const Notification = NotificationOpen.notification;
+        var seen = [];
+        alert(
+          JSON.stringify(Notification.data, function(key, val) {
+            if (val != null && typeof val == "object") {
+              if (seen.indexOf(val) >= 0) {
+                return;
+              }
+              seen.push(val);
+            }
+            return val;
+          })
+        );
+        firebase
+          .notifications()
+          .removeDeliveredNotification(Notification.notificationId);
+      });
+  }
+  componentWillUnmount() {
+    this.notificationDisplayedListener();
+    this.notificationListener();
+    this.notificationOpenedListener();
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Hello to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
@@ -23,18 +95,18 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
--- android/app/src/main/java/com/rnfirebasenotification/MainApplication.java
+++ android/app/src/main/java/com/rnfirebasenotification/MainApplication.java
@@ -3,11 +3,13 @@ package com.rnfirebasenotification;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage; 
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // firebase-message
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage; // firebase-notification
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage()
+          new RNFirebasePackage(),
+          new RNFirebaseMessagingPackage(),
+          new RNFirebaseNotificationsPackage()
       );
     }
 
